12775f12162fdb3f8de256392b381bf5ac85c72e
alexandrb <alexandrbury@gmail.com>
2024-12-01 18:21:14 +0300

    Solved tasks

--- src/main/java/com/ifortex/bookservice/controller/BookController.java
+++ src/main/java/com/ifortex/bookservice/controller/BookController.java
@@ -7,12 +7,14 @@ import lombok.AllArgsConstructor;
 import org.springframework.lang.Nullable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
-@RestController("api/v1/books")
+@RestController
+@RequestMapping("api/v1/books")
 @AllArgsConstructor
 public class BookController {
 
--- src/main/java/com/ifortex/bookservice/controller/MemberController.java
+++ src/main/java/com/ifortex/bookservice/controller/MemberController.java
@@ -4,11 +4,13 @@ import com.ifortex.bookservice.model.Member;
 import com.ifortex.bookservice.service.MemberService;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController("api/v1/members")
+@RestController
+@RequestMapping("api/v1/members")
 @AllArgsConstructor
 public class MemberController {
 
